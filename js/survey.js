ca7997799f8380e4785a23c5f30a1f419e533c88
adiqqo <adriankorkus9@gmail.com>
2020-02-28 02:32:59 +0100

    Rect detection

--- js/survey.js
+++ js/survey.js
@@ -1,4 +1,14 @@
+class Coordinate {
+    constructor(x, y, number, value) {
+        this.x = x;
+        this.y = y;
+        this.number = number;
+        this.value = value;
+    }
+}
+
 let globalCtx;
+let coordinates = [];
 
 function createCanvas(width, height) {
     const canvas = document.createElement('canvas');
@@ -36,8 +46,17 @@ function loadTemplate(path) {
 
 function templateMatching(source, template) {
     let best = iterate(source, template);
-    globalCtx.strokeStyle = 'green';
+    globalCtx.strokeStyle = 'red';
     globalCtx.strokeRect(best.x, best.y, template.width, template.height);
+    return best;
+}
+
+function templateMatching2(source, template,addx, addy) {
+    let bests = iterate2(source, template);
+    globalCtx.strokeStyle = 'green';
+    globalCtx.fillRect(bests[0].x+addx, bests[0].y+addy, template.width, template.height);
+    globalCtx.fillRect(bests[1].x+addx, bests[1].y+addy, template.width, template.height);
+    //return best;
 }
 
 function calculateSqDiffGray(source, xOffset, yOffset, template, best) {
@@ -47,7 +66,7 @@ function calculateSqDiffGray(source, xOffset, yOffset, template, best) {
             let m = (y * template.width + x);
             let j = m * 4;
             let i = ((y + yOffset) * source.width + (x + xOffset)) * 4;
-            sum += Math.pow((template.data[j] - source.data[i]), 2);
+            sum += Math.pow((template.data[j] - source.data[i]+template.data[j+1] - source.data[i+1]+template.data[j+2] - source.data[i+2]), 2);
             if (sum > best.value || sum > 100000000) {
                 return 999999999;
             }
@@ -55,6 +74,25 @@ function calculateSqDiffGray(source, xOffset, yOffset, template, best) {
     }
     return sum;
 };
+
+function iterate2(source, template) {
+    let bests = [];
+    let best = { x: null, y: null, value: 99999999999 };
+    let best2 = { x: null, y: null, value: 99999999999 };
+    for (let y = 0; y < source.height - template.height + 1; ++y) {
+        for (let x = 0; x < source.width - template.width + 1; ++x) {
+            let value = calculateSqDiffGray(source, x, y, template, best);
+
+            if (value < best.value) {
+                best2 = Object.assign({}, best);
+                best = { x: x, y: y, value: value }
+            }
+        }
+    }
+    bests.push(best);
+    bests.push(best2);
+    return bests;
+}
 
 function iterate(source, template) {
     let best = { x: null, y: null, value: 99999999999 };
@@ -67,7 +105,6 @@ function iterate(source, template) {
             }
         }
     }
-    console.log(best)
     return best;
 }
 
@@ -79,19 +116,57 @@ function getNameOfTemplates() {
     });
 }
 
-getNameOfTemplates().then(function (nameOfTemplates) {
-    $.getJSON('/api/getNameOfImages', function (data) {
-        console.log(data);
-        data.forEach(element => {
-            const pathToImages = "scans/" + element.name;
-            loadImage(pathToImages).then(function (source) {
-                nameOfTemplates.forEach(nameOfTemplate => {
-                    const pathToTemplate = "templates/" + nameOfTemplate.name;
-                    loadTemplate(pathToTemplate).then(function (template) {
-                        templateMatching(source, template);
+
+function initSystem() {
+    return new Promise((resolve, reject) => {
+        getNameOfTemplates().then(function (nameOfTemplates) {
+            $.getJSON('/api/getNameOfImages', function (data) {
+                data.forEach(element => {
+                    const pathToImages = "scans/" + element.name;
+                    loadImage(pathToImages).then(function (source) {
+                        nameOfTemplates.forEach(nameOfTemplate => {
+                            const pathToTemplate = "templates/" + nameOfTemplate.name;
+                            loadTemplate(pathToTemplate).then(function (template) {
+                                let result = templateMatching(source, template);
+                                coordinates.push(new Coordinate(result.x, result.y, parseInt(nameOfTemplate.name), ''));
+                            });
+                        })
+                        resolve(coordinates);
                     });
-                })
+                });
             });
         });
     });
+}
+function imgTester(template){
+	const canvas = document.createElement('canvas');
+	canvas.width  = template.width;
+	canvas.height = template.height;
+	document.body.appendChild(canvas);
+	const ctx = canvas.getContext('2d');
+	const imgData=ctx.createImageData(template.width,template.height);
+
+	for (let i=0;i<imgData.data.length;i+=4){
+			  imgData.data[i+0] = template.data[i];
+			  imgData.data[i+1]= template.data[i+1];
+			  imgData.data[i+2]= template.data[i+2];
+			  imgData.data[i+3]= template.data[i+3];
+	}
+
+	ctx.putImageData(imgData,0,0);
+	return ctx;
+}
+
+
+initSystem().then(function (coordinates) {
+    const pathToTemplate = "checkTemplate/" + "checkTemplate.png";
+    loadTemplate(pathToTemplate).then(function(template) {
+        for(let i = 0; i < coordinates.length-1; i++){
+            let image = globalCtx.getImageData(coordinates[i].x, coordinates[i].y, 
+                coordinates[i+1].x-coordinates[i].x+100 , coordinates[i+1].y-coordinates[i].y);
+            templateMatching2(image, template, coordinates[i].x, coordinates[i].y );
+            console.log(i);
+        }
+    });
+    
 });
--- server.js
+++ server.js
@@ -6,7 +6,21 @@ const port = 3000;
 
 app.use('/js', express.static(path.join(__dirname, 'js')))
 app.use('/scans', express.static(path.join(__dirname, 'scans')))
+app.use('/checkTemplate', express.static(path.join(__dirname, 'checkTemplate')))
 app.use('/templates', express.static(path.join(__dirname, 'templates')))
+
+function compare(a, b) {
+    const nameA = parseInt(a.name);
+    const nameB = parseInt(b.name);
+  
+    let comparison = 0;
+    if (nameA > nameB) {
+      comparison = 1;
+    } else if (nameA < nameB) {
+      comparison = -1;
+    }
+    return comparison;
+  }
 
 function readDir(pathToFolder, res){
     let nameOfFiles = []
@@ -17,6 +31,7 @@ function readDir(pathToFolder, res){
         files.forEach(function (file) {
             nameOfFiles.push({ name: file });
         });
+        nameOfFiles.sort(compare);
         res.contentType('application/json');
         res.send(JSON.stringify(nameOfFiles));
     });
